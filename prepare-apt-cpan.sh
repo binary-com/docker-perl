059faa6877014f9c229a94c48ef0a33aadef5520
ea-binay <eyad@regentmarkets.com>
2021-02-04 17:31:47 +0800

    install submoduels from 'vendors' directory

--- prepare-apt-cpan.sh
+++ prepare-apt-cpan.sh
@@ -8,7 +8,26 @@ if [ -r /app/aptfile ]; then
     apt-get -y -q update
     apt-get -y -q --no-install-recommends install $(cat /app/aptfile)
 fi
+
 cpanm --notest --installdeps .
+
+rm -r ~/.cpanm
+
+if [ -d /app/vendors ]; then
+    for dir in /app/vendors/*; do
+        if [ ~ `ls -A $dir` ]; then
+            echo -e "It seems that your git submodules are not initialized correctly\nif you are not using submodules please delete 'vendors' subdirctory" 1>&2
+            exit 1
+        fi
+        cd $dir
+        dzil authordeps --missing | cpanm
+        cpanm --notest --installdeps .
+        rm -r ~/.cpanm
+        dzil install
+        dzil clean
+    done
+fi
+
 apt-get purge -y -q $(perl -le'@seen{split " ", "" . do { local ($/, @ARGV) = (undef, "/app/aptfile"); <> }} = () if -r "aptfile"; print for grep { !exists $seen{$_} } qw(make gcc git openssh-client libc6-dev libssl-dev zlib1g-dev patch)')
 rm -rf /var/lib/apt/lists/* /var/cache/apt/* /root/.cpanm /tmp/*
 
